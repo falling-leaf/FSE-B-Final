9cb24979883ca7fca8928e720f8629eba2ff9049
Liu Yijie <1148781323@qq.com>
2024-06-13 10:08:17 +0800

    贷款纠错

--- backend/onlinebank/loan/loanmanager_urls.py
+++ backend/onlinebank/loan/loanmanager_urls.py
@@ -2,9 +2,9 @@ from . import manager_views
 from django.urls import path
 
 urlpatterns = [
-    path('showAllLoanApplicationUnlent', manager_views.showAllLoanApplicationUnlent, name='show_all_loan_application_unlent'),
-    path('lenderLoanApplication', manager_views.lenderLoanApplication, name='lender_loan_application'),
-    path('unrepayReminderManager', manager_views.unrepayReminderManager),
+    path('showAllLoanApplicationUnlent/', manager_views.showAllLoanApplicationUnlent, name='show_all_loan_application_unlent'),
+    path('lenderLoanApplication/', manager_views.lenderLoanApplication, name='lender_loan_application'),
+    path('unrepayReminderManager/', manager_views.unrepayReminderManager),
 ]
 
 from . import models
